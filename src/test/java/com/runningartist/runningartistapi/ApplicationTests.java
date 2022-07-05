8fa901010d398cf1d7f2bf40486f566349074fad
David Reategui <reateguicevallos.darh@gmail.com>
2022-07-05 08:37:20 -0500

    Initial commit

---
+++ src/test/java/com/runningartist/runningartistapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runningartist.runningartistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
